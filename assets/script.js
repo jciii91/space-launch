dde035219ada5693b91d21e9f470546781a3863f
JCIII <jcostello711@gmail.com>
2022-02-10 21:34:48 -0500

    no update, just saving work in progress

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,27 @@
-fetch ("https://lldev.thespacedevs.com/2.2.0/launch/?limit=100&window_start__gte=2022-02-01T00%3A00%3A00Z&window_start__lte=2022-03-01T00%3A00%3A00Z")
+todaysDate = moment().format("YYYY-MM-DD");
+thisYear =  parseInt(todaysDate.slice(0,4));
+thisMonth = parseInt(todaysDate.slice(5,7));
+launchMonths = [];
+badDatesArray = [];
+
+function getLaunches() {
+    if (thisMonth < 10) {
+        lastDate = thisYear + "-0" + (thisMonth + 1) + "-01";
+    } else {
+        lastDate = thisYear + "-" + (thisMonth + 1) + "-01";
+    }
+
+    if (thisMonth == 12) {
+        lastDate = (thisYear + 1) + "-01-01";
+    }
+
+    launchMonths.push(getLaunchArray(todaysDate,lastDate));
+
+    var yearIndex = thisYear;
+}
+
+function getLaunchArray(first,last) {
+    fetch ("https://lldev.thespacedevs.com/2.2.0/launch/?limit=100&window_start__gte=" + first + "T00%3A00%3A00Z&window_start__lte=" + last + "T00%3A00%3A00Z")
     .then(response => response.json())
     .then(data => {
         for (var i = 0; i < data.count; i++) {
@@ -6,8 +29,7 @@ fetch ("https://lldev.thespacedevs.com/2.2.0/launch/?limit=100&window_start__gte
             console.log(launches[i].window_start.slice(0,10));
         }
     })
-
-var array = ["2022-02-14","2022-02-16","2022-02-18"];
+}
 
 $( "#datePicker" ).datepicker({
     showOn: "button",
@@ -17,3 +39,5 @@ $( "#datePicker" ).datepicker({
         return [ array.indexOf(string) == -1 ]
     }
 });
+
+getLaunches();
