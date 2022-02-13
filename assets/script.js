727372902aa6b5adab66e5d1ae168b323023e3c0
Hilary V <92323804+hvasconez24@users.noreply.github.com>
2022-02-13 09:32:22 -0500

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,137 +1,61 @@
-var todaysDate = moment().format("YYYY-MM-DD");
-var thisYear =  parseInt(todaysDate.slice(0,4));
-var thisMonth = parseInt(todaysDate.slice(5,7));
-var badDatesArray = [];
+L.mapquest.key = 'lCzrKhevQBbPeLBz5rv7JR';
 
-var currentYear;
-var currentMonth;
+// 'map' refers to a <div> element with the ID map
 
-function getMap(lat,lon) {
-    fetch("https://www.mapquestapi.com/staticmap/v5/map?key=lCzrKhevQBbPeLBz5rv7JRWrlYKVnVae&center=" + lat + ","+ lon)
-    .then(response => {
-        console.log(response);
-        document.getElementById("map").setAttribute("src",response.url);
-    })
-}
 
-function getBadDates(inputYear,inputMonth) {
-    badDatesArray = [];
-    var maxDay = 31;
+let launches = [];
+let limit  = 100;
 
-    switch(inputMonth) {
-        case "January":
-            month = "01";
-            nextMonth = "02"
-            nextYear = inputYear;
-            maxDay = 31;
-            break;
-        case "February":
-            month = "02";
-            nextMonth = "03"
-            nextYear = inputYear;
-            maxDay = 28;
-            if (inputYear % 4 == 0) {maxDay = 29}
-            break;
-        case "March":
-            month = "03";
-            nextMonth = "04"
-            nextYear = inputYear;
-            maxDay = 31;
-            break;
-        case "April":
-            month = "04";
-            nextMonth = "05"
-            nextYear = inputYear;
-            maxDay = 30;
-            break;
-        case "May":
-            month = "05";
-            nextMonth = "06"
-            nextYear = inputYear;
-            maxDay = 31;
-            break;
-        case "June":
-            month = "06";
-            nextMonth = "07"
-            nextYear = inputYear;
-            maxDay = 30;
-            break;
-        case "July":
-            month = "07";
-            nextMonth = "08"
-            nextYear = inputYear;
-            maxDay = 31;
-            break;
-        case "August":
-            month = "08";
-            nextMonth = "09"
-            nextYear = inputYear;
-            maxDay = 31;
-            break;
-        case "September":
-            month = "09";
-            nextMonth = "10"
-            nextYear = inputYear;
-            maxDay = 30;
-            break;
-        case "October":
-            month = "10";
-            nextMonth = "11"
-            nextYear = inputYear;
-            maxDay = 31;
-            break;
-        case "November":
-            month = "11";
-            nextMonth = "12"
-            nextYear = inputYear;
-            maxDay = 30;
-            break;
-        case "December":
-            month = "12";
-            nextMonth = "01"
-            nextYear = parseInt(inputYear) + 1;
-            maxDay = 31;
-            break;
-    }
+let mapKey = '';
 
-    fetch ("https://lldev.thespacedevs.com/2.2.0/launch/?limit=100&window_start__gte=" + inputYear + "-" + month + "-01T00%3A00%3A00Z&window_start__lt=" + nextYear + "-" + nextMonth + "-01T00%3A00%3A00Z")
-    .then(response => response.json())
+const loadData = function(launchDate) {
+    let api = `https://lldev.thespacedevs.com/2.2.0/launch/?limit=${limit}&window_start__gte=${launchDate}`;
+    fetch(api).then(response => response.json())
     .then(data => {
-        for (var i = 0; i < data.count; i++) {
-            launches = data.results;
-            badDatesArray.push(launches[i].window_start.slice(0,10));
+        launches = data.results;
+        const map = L.mapquest.map('map', {
+            center: [37.7749, -122.4194],
+            layers: L.mapquest.tileLayer('map'),
+            zoom: 6,
+            
+            });
+        for(let i=0; i < launches.length; i++) {
+            let latitude = launches[i].pad.latitude;
+            let longitude = launches[i].pad.longitude;
+            let name = launches[i].name;
+            let description = launches[i].mission.description;
+            let imageurl = launches[i].image;
+            L.marker([latitude, longitude], {
+                icon: L.mapquest.icons.marker(),
+                draggable: false
+              }).bindPopup(name).addTo(map).on("click", function(){
+                  document.getElementById("info").innerHTML = `
+                  <h4>Name : ${name}</h4>
+                    <h6><strong>Description</strong></h6>
+                    <p>${description}</p>
+                    <div>
+                        <img src="${imageurl}" alt="" width="100%">
+                    </div>
+                  `;
+              });
         }
-        
-        calendar = document.createElement("input");
-        calendar.setAttribute("type","input");
-        calendar.setAttribute("id","datePicker");
-        calendar.setAttribute("class","datepicker");
-
-        document.getElementById("calendarHolder").appendChild(calendar);
-
-        var calID = $( "#datePicker" )
-
-        calID.datepicker({
-            minDate: month + "/01/" + year,
-            maxDate: month + "/" + maxDay + "/" + year,
-            beforeShowDay: function(date){
-                var string = jQuery.datepicker.formatDate('yy-mm-dd', date);
-                var array = badDatesArray;
-                return [ array.indexOf(string) != -1 ];
-            },
-            onClose: function(date){
-                output = date.slice(6,10) + "-" + date.slice(0,2) + "-" + date.slice(3,5);
-                console.log(output);
-                calID.remove();
-            }
-        });
-
-        calID[0].focus();
     })
 }
 
-$("#setMonth").on("click",function(){
-    year = $("#launchYear")[0].value;
-    month = $("#monthsList")[0].value;
-    getBadDates(year,month);
+
+
+$(function() {
+    $( "#datepicker" ).datepicker( {
+        minDate: 0,
+        dateFormat: "yy-mm-ddT00:00:00Z",
+        onSelect: function(date, picker){
+            console.log(date)
+            document.getElementById("saved-searches").innerHTML = 
+            document.getElementById("saved-searches").innerHTML +
+             `<button class="saved-search">${date}</button>`;
+            loadData(date);
+        }
+    });
 });
+
+
